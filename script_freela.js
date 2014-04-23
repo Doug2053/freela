33c98f42a60bc6d50913884684d3e2cb899888df
Doug2053 <dg.silva@outlook.com>
2014-04-23 14:01:41 -0300

    atualização

--- script_freela.js
+++ script_freela.js
@@ -15,4 +15,50 @@ $(document).ready(function(){
 	);
 });
 
+function hora(){
+	var hora = new Date().toLocaleString().split(" ")[1];
+	
+	return hora
+}
 
+function btnInicio(){
+	document.getElementById("bgn").value = hora();
+}
+
+function btnIntervalo(){
+	var stringBgn = document.getElementById("bgn").value;
+	if ((stringBgn == null)||(stringBgn == "")){
+		alert("Inicie a Contagem!");
+		} else {
+			document.getElementById("int").value = hora();
+		}
+}
+
+function btnRetorno(){
+	var stringInt = document.getElementById("int").value;
+	if((stringInt == null) || (stringInt == "") || (stringInt == "Inicie a Contagem!")){
+		alert("Inicie a Contagem!");
+	} else {
+	document.getElementById("bck").value = hora();
+	}
+}
+
+function btnSaida(){
+	var stringBgn = document.getElementById("bgn").value;
+	if ((stringBgn == null)||(stringBgn == "")){
+		alert("Inicie a Contagem!");
+		} else {
+			document.getElementById("out").value = hora();
+		}
+}
+
+function btnSalvar(){
+	var stringBgn = document.getElementById("bgn").value;
+	var stringOut = document.getElementById("out").value;
+	
+	if (((stringBgn == null)||(stringBgn =="")) && ((stringOut == null)||(stringOut == ""))){
+		alert("É necessário ao menos um Início e uma Saída!");
+	} else {
+		
+	}
+}
